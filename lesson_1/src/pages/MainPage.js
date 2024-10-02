6f9eef87221acfaab0fd0c5e3831dd0b1459df73
Эсенбеков Даниэль <cflsrjdv@gmail.com>
2024-10-02 22:16:16 +0600

    first commit

---
+++ lesson_1/src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from "../components/Button";
+
+const MainPage = () => {
+    return (
+        <div>
+            <h1>Hello mainpage</h1>
+            <Button text='add'/>
+        </div>
+    );
+};
+
+export default MainPage;
